223ccb66b20820a48373c461c55d0cf584a17ba2
Hiro Matsuoka <kuronekodaisuki@me.com>
2015-07-24 04:13:43 +0000

    zoom sample

--- zoom.cpp
+++ zoom.cpp
@@ -27,10 +27,10 @@ bool _SDL_Zoom_FP(SDL_Surface *src, SDL_Surface *dst, double scale)
         switch (bitsPerPixel)
         {
         case 24:
-                for (int y = 0; y < dst->y; y++)
+                for (int y = 0; y < dst->h; y++)
                 {
                         FIXED_POINT_t X = toFP(0);
-                        for (int x = 0; x < dst->x; x++)
+                        for (int x = 0; x < dst->w; x++)
                         {
                                 BiLinear24_FP_NEON(src, X, Y, dst, x, y);
                                 X += dX;
@@ -45,11 +45,17 @@ bool _SDL_Zoom_FP(SDL_Surface *src, SDL_Surface *dst, double scale)
 int main(int argc, char *argv[])
 {
         SDL_Init(SDL_INIT_EVERYTHING);
-        SDL_Surface *screen = SDL_SetVideoMode(100, 100, 24, SDL_HWSURFACE | SDL_DOUBLEBUF);
-	SDL_Surface *src = SDL_CreateRGBSurface(SDL_SWSURFACE, 40, 40, 24, 0x00ff0000, 0x0000ff00, 0x000000ff, 0);
-	SDL_Surface *dst = SDL_CreateRGBSurface(SDL_SWSURFACE, 40, 40, 24, 0x00ff0000, 0x0000ff00, 0x000000ff, 0);
-	SDL_Rect srcRect = {0, 0, src->w, src->h};
+        const char *filename = "cat.bmp";
+        SDL_Surface *src = SDL_LoadBMP(filename);
+	double scale = 1.5;
+
+        SDL_Surface *screen = SDL_SetVideoMode(src->w * scale, src->h * scale, 24, SDL_HWSURFACE | SDL_DOUBLEBUF);
+	SDL_Surface *dst = SDL_CreateRGBSurface(SDL_SWSURFACE, src->w * scale, src->h * scale, src->format->BitsPerPixel,
+                        src->format->Rmask, src->format->Gmask, src->format->Bmask, 0);
+
+	SDL_Rect srcRect = {0, 0, dst->w, dst->h};
         SDL_Rect dstRect = {0, 0};
+	_SDL_Zoom_FP(src, dst, scale);
 	SDL_BlitSurface(dst, &srcRect, screen, &dstRect);
 	SDL_Flip(screen);
 	bool quit = false;
